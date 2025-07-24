4b7726095d01bb7acd3f2604433c7b54cec18b5d
Mehul Mohan <mehulmpt@gmail.com>
2025-07-24 16:56:08 +0530

    fix: lower target to es2018

---
+++ .changeset/seven-buckets-camp.md
@@ -0,0 +1,5 @@
+---
+'@fermion-app/sdk': minor
+---
+
+target es2018 for webpack 4
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/*.ts', '!src/**/*.test.ts'],
+	entry: ['src/index.ts', 'src/recorded-video.ts', 'src/livestream-video.ts', 'src/test.ts'],
 	format: ['cjs', 'esm'],
 	dts: true,
 	outDir: 'dist',
 	clean: true,
+	target: 'es2018',
 	ignoreWatch: ['**/*.test.ts'],
 	esbuildOptions(options) {
 		options.platform = 'neutral'
